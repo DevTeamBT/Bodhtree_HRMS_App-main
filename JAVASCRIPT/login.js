a23fc7c2213d02a8e3f9b0402859241414b49b65
priyanka <Mpriyanka@bodhtree.com>
2024-09-13 11:45:59 +0530

    admin and manager dir path changed

--- JAVASCRIPT/login.js
+++ JAVASCRIPT/login.js
@@ -33,11 +33,11 @@ async function loginUser(event) {
 
             const userRole = result.user.roleName;
             if (userRole === 'admin') {
-                window.location.replace('/HTML/HrPages');
+                window.location.replace('/HTML/HrPages/hr.html');
             } else if (userRole === 'employee') {
                 window.location.replace('/HTML/employeePages/home.html');
             } else if (userRole === 'manager') {
-                window.location.replace('/HTML/managerPages/manager.html');
+                window.location.replace('/HTML/managerPages/managerHome.html');
             } else {
                 resultElement.innerText = 'Invalid role';
             }
